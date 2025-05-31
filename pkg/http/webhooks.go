2fafb7e7eb4f75714156445fe89731c33676000a
Daniel Abraham <aeroquix@gmail.com>
2025-05-31 15:27:52 -0700

    Passthrough handler

--- pkg/http/webhooks.go
+++ pkg/http/webhooks.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"context"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -20,8 +22,8 @@ const (
 )
 
 type httpServer struct {
-	httpPort   int    // To initialize the HTTP server.
-	thrippyURL string // Optional passthrough for Thrippy OAuth.
+	httpPort   int      // To initialize the HTTP server.
+	thrippyURL *url.URL // Optional passthrough for Thrippy OAuth.
 
 	thrippyAddr  string // To communicate with Thrippy via gRPC.
 	thrippyCreds credentials.TransportCredentials
@@ -37,12 +39,11 @@ func newHTTPServer(cmd *cli.Command) *httpServer {
 	}
 }
 
-// baseURL converts the given address (e.g. "localhost:14460")
-// into a URL with an HTTP scheme and without any suffix, for Thrippy.
-// If the address is empty, this function also returns an empty string.
-func baseURL(addr string) string {
+// baseURL converts the given address (e.g. "localhost:14460") into a URL.
+// If the address is empty, this function returns a nil reference.
+func baseURL(addr string) *url.URL {
 	if addr == "" {
-		return ""
+		return nil
 	}
 
 	// Force an HTTP scheme.
@@ -56,21 +57,28 @@ func baseURL(addr string) string {
 	// Strip any suffix after the address.
 	u, err := url.Parse(addr)
 	if err != nil {
-		return ""
+		return nil
 	}
 	if u.Host == "" {
-		return ""
+		return nil
 	}
 	u.Path = ""
 	u.RawQuery = ""
 	u.Fragment = ""
 
-	return u.String()
+	return u
 }
 
 // run starts an HTTP server to expose webhooks.
 // This is blocking, to keep the Omdient server running.
 func (s *httpServer) run() error {
+	if s.thrippyURL != nil {
+		log.Info().Msgf("HTTP passthrough for Thrippy OAuth callbacks: %s", s.thrippyURL)
+		http.HandleFunc("GET /callback", s.thrippyHandler)
+		http.HandleFunc("GET /start", s.thrippyHandler)
+		http.HandleFunc("POST /start", s.thrippyHandler)
+	}
+
 	server := &http.Server{
 		Addr:         net.JoinHostPort("", strconv.Itoa(s.httpPort)),
 		ReadTimeout:  timeout,
@@ -85,4 +93,51 @@ func (s *httpServer) run() error {
 	}
 
 	return nil
+}
+
+// thrippyHandler passes-through incoming HTTP requests (OAuth callbacks),
+// as a proxy, to a local Thrippy server. This allows Omdient and Thrippy to
+// share a single HTTP tunnel when running together in a local development setup.
+func (s *httpServer) thrippyHandler(w http.ResponseWriter, r *http.Request) {
+	l := log.With().Str("http_method", r.Method).Str("url_path", r.URL.EscapedPath()).Logger()
+	l.Info().Msg("passing-through HTTP request to Thrippy")
+
+	// Adjust the original URL to the Thrippy server's base URL.
+	u := r.URL
+	u.Scheme = s.thrippyURL.Scheme
+	u.Host = s.thrippyURL.Host
+
+	// Construct the proxy request.
+	ctx, cancel := context.WithTimeout(r.Context(), timeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, r.Method, u.String(), r.Body)
+	if err != nil {
+		l.Err(err).Msg("failed to construct Thrippy proxy request")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	req.Header = r.Header.Clone()
+
+	// Send the proxy request.
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		l.Err(err).Msg("failed to send Thrippy proxy request")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Relay Thrippy's response back to the client.
+	for k, vs := range resp.Header {
+		for _, v := range vs {
+			w.Header().Add(k, v)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	if _, err := io.Copy(w, resp.Body); err != nil {
+		l.Err(err).Msg("failed to copy Thrippy response body")
+	}
 }
--- pkg/http/webhooks_test.go
+++ pkg/http/webhooks_test.go
@@ -1,6 +1,10 @@
 package http
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
 	"testing"
 )
 
@@ -8,45 +12,182 @@ func TestBaseURL(t *testing.T) {
 	tests := []struct {
 		name string
 		addr string
-		want string
+		want *url.URL
 	}{
 		{
 			name: "empty",
-			addr: "",
-			want: "",
 		},
 		{
 			name: "default_without_scheme",
 			addr: "localhost:14460",
-			want: "http://localhost:14460",
+			want: parseURL("http://localhost:14460"),
 		},
 		{
 			name: "addr_with_http_scheme",
 			addr: "http://host:1234",
-			want: "http://host:1234",
+			want: parseURL("http://host:1234"),
 		},
 		{
 			name: "addr_with_https_scheme",
 			addr: "https://test.com",
-			want: "http://test.com",
+			want: parseURL("http://test.com"),
 		},
 		{
 			name: "addr_with_path",
 			addr: "https://addr/foo/bar",
-			want: "http://addr",
+			want: parseURL("http://addr"),
 		},
 		{
 			name: "invalid_addr",
 			addr: "host:port",
-			want: "",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := baseURL(tt.addr); got != tt.want {
-				t.Errorf("baseURL() = %q, want %q", got, tt.want)
+			if got := baseURL(tt.addr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("baseURL() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func parseURL(rawURL string) *url.URL {
+	u, _ := url.Parse(rawURL)
+	return u
+}
+
+func TestHTTPServerThrippyHandler(t *testing.T) {
+	tests := []struct {
+		name        string
+		reqMethod   string
+		reqURL      string
+		wantQuery   url.Values
+		thrippyResp *http.Response
+		wantResp    *http.Response
+	}{
+		{
+			name:      "no_thrippy_server",
+			reqMethod: http.MethodGet,
+			reqURL:    "https://example.com",
+			wantResp: &http.Response{
+				StatusCode: http.StatusInternalServerError,
+				Header:     http.Header{},
+			},
+		},
+		{
+			name:      "ok",
+			reqMethod: http.MethodGet,
+			reqURL:    "https://example.com",
+			thrippyResp: &http.Response{
+				StatusCode: http.StatusOK,
+			},
+			wantResp: &http.Response{
+				StatusCode: http.StatusOK,
+				Header: http.Header{
+					"Content-Length": []string{"0"},
+				},
+			},
+		},
+		{
+			name:      "accepted_with_data",
+			reqMethod: http.MethodGet,
+			reqURL:    "https://example.com?k1=v1&k2=v2",
+			wantQuery: map[string][]string{"k1": {"v1"}, "k2": {"v2"}},
+			thrippyResp: &http.Response{
+				StatusCode: http.StatusAccepted,
+				Header: http.Header{
+					"X-Test-Header": []string{"3", "4"},
+				},
+			},
+			wantResp: &http.Response{
+				StatusCode: http.StatusAccepted,
+				Header: http.Header{
+					"Content-Length": []string{"0"},
+					"X-Test-Header":  []string{"3", "4"},
+				},
+			},
+		},
+		{
+			name:      "redirect_to_error",
+			reqMethod: http.MethodGet,
+			reqURL:    "https://example.com/redirect",
+			thrippyResp: &http.Response{
+				StatusCode: http.StatusBadRequest,
+			},
+			wantResp: &http.Response{
+				StatusCode: http.StatusBadRequest,
+				Header: http.Header{
+					"Content-Length": []string{"0"},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Initialize mock Thrippy server.
+			s := httptest.NewUnstartedServer(mockThrippyServer(t, tt.wantQuery, tt.thrippyResp))
+			thrippyBaseURL := "http://127.0.0.1:0"
+			if tt.thrippyResp != nil {
+				s.Start()
+				thrippyBaseURL = s.URL
+			}
+			defer s.Close()
+
+			u, _ := url.Parse(thrippyBaseURL)
+			server := &httpServer{thrippyURL: u}
+
+			// Construct client request.
+			w := httptest.NewRecorder()
+			r := httptest.NewRequestWithContext(t.Context(), tt.reqMethod, tt.reqURL, http.NoBody)
+			r.Header.Add("X-Test-Header", "1")
+			r.Header.Add("X-Test-Header", "2")
+
+			// Send client request to handler under test.
+			server.thrippyHandler(w, r)
+			got := w.Result()
+
+			// Response checks.
+			if got.StatusCode != tt.wantResp.StatusCode {
+				t.Errorf("response status code: got %v, want %v", got.StatusCode, tt.wantResp.StatusCode)
+			}
+
+			got.Header.Del("Date")
+			if !reflect.DeepEqual(got.Header, tt.wantResp.Header) {
+				t.Errorf("response headers: got %v, want %v", got.Header, tt.wantResp.Header)
+			}
+		})
+	}
+}
+
+func mockThrippyServer(t *testing.T, wantQuery url.Values, resp *http.Response) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Support redirection in tests.
+		if r.URL.Path == "/redirect" {
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+
+		// Request checks.
+		hs := r.Header.Values("X-Test-Header")
+		if len(hs) != 2 || hs[0] != "1" || hs[1] != "2" {
+			t.Errorf("thrippy request headers: got %v, want [1 2]", hs)
+		}
+
+		if wantQuery == nil {
+			wantQuery = url.Values{}
+		}
+		if gotQuery := r.URL.Query(); !reflect.DeepEqual(gotQuery, wantQuery) {
+			t.Errorf("thrippy request query: got %v, want %v", gotQuery, wantQuery)
+		}
+
+		// Construct response.
+		for k, vs := range resp.Header {
+			for _, v := range vs {
+				w.Header().Add(k, v)
+			}
+		}
+		w.WriteHeader(resp.StatusCode)
+	})
 }
